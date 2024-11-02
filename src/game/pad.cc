6f4f6debb51223005730daeee49b847dbfd75130
Maicol Castro <maicolcastro.abc@gmail.com>
2024-11-02 09:31:29 -0300

    add a license

--- src/game/pad.cc
+++ src/game/pad.cc
@@ -1,4 +1,7 @@
 // Copyright 2024 Maicol Castro (maicolcastro.abc@gmail.com).
+// Distributed under the BSD 3-Clause License.
+// See LICENSE.txt in the root directory of this project
+// or at https://opensource.org/license/bsd-3-clause.
 
 #include <luna/game/pad.hh>
 #include <luna/game/main.hh>
